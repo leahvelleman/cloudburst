18fac3639b09677fffb51118e43d2cb6ec246733
leahvelleman <leahvelleman@gmail.com>
2017-07-10 18:12:40 -0400

    Switched to Pynini

--- extensions/language.py
+++ extensions/language.py
@@ -1,6 +1,9 @@
 """ Language, Level, and Form objects
 """
-import hfst
+import pynini
+from pynini import union as u
+from pynini import transducer as t
+from pynini import acceptor as a
 
 class Level(object):
     """A level of representation for language forms.
@@ -12,64 +15,69 @@ class Level(object):
     Attributes:
         name: A valid Python identifier naming the level. Level names must be
             unique per language.
-        sigil: A sigil that surrounds markup text meant to be interpreted as a
-            form at this level.
-        children: A list of levels that are defined in terms of this one.
+        derivation: If this is the root level of its language, a pynini
+            acceptor constituting the root lexicon. If this is a child level, a
+            pynini transducer deriving representations at this level from
+            representations at the parent level.
         parent: The parent level that this one is defined in terms of; `None`
             if this is the root level of its language.
+        children: A list of levels that are defined in terms of this one.
         converters: If thislevel and otherlevel are levels in the same language,
             ``thislevel.converters[otherlevel]`` is a path from thislevel to
             otherlevel running through the root node of the language.
         language: Language this level belongs to.
     """
 
+    # pylint: disable=too-many-instance-attributes
+
     def __init__(self,
                  name: str,
-                 sigil: str,
-                 language: 'Language') -> None:
-        if language.has_sigil(sigil):
-            raise Error("Sigil %s in use in language %s" % (sigil, language))
-        if language.has_name(name):
-            raise Error("Level name %s in use or reserved in language %s" %
-                        (name, language))
+                 derivation: pynini.Fst,
+                 parent: 'Level' = None) -> None:
         self.name = name
-        self.sigil = sigil
-        self.language = language
-
         self.children = []
-        self.parent = None
-        self.converters = {self: hfst.regex("?")}
-
-        language.register_level(level=self, name=name, sigil=sigil)
+        self.derivation = derivation
+        # TODO: convince self that this derivation isn't getting mutated ever
+        # -- it might be useful someday to have the unmutated, uncomposed
+        # original on hand.
+        if parent:
+            self.language = parent.language
+            # TODO add test: error or at least warn if the derivation for a
+            # child node isn't a transducer
+            if self.language.has_name(name):
+                raise Error("Level name %s in use or reserved in language %s." %
+                            (name, self.language))
+            self.parent = parent
+            self.language.register_level(self)
+        else:
+            self.language = self
+            self.parent = None
+            # TODO add test: error if the "derivation" for a root node isn't an
+            # acceptor.
+        self.converters = {self: derivation.copy().project(project_output=True)}
 
     def __repr__(self):
-        return "%s (sigil %s)" % (self.name, self.sigil)
-
-    def __call__(self, text):
-        if not self.parent:
-            raise Error("Level %s called before being connected to a language."
-                        % self.name)
-        return text
+        return self.name
 
     def add_child(self,
                   name: str,
-                  sigil: str,
-                  derivation: hfst.HfstTransducer) -> None:
+                  derivation: pynini.Fst) -> None:
         """Add the specified node as a child of mine, and set up converters
         between it and all the nodes I can convert to or from."""
-        child = Level(name=name, sigil=sigil, language=self.language)
-        child.parent = self
+        child = Level(name=name, parent=self,
+                      derivation=derivation)
         self.children.append(child)
+        self.__connect_child(child)
+
+    def __connect_child(self, child: 'Level') -> None:
+        """For every node I can convert to or from, set up converters for the
+        specified child node."""
+        derivation = child.derivation
         for target in list(self.converters):
-            # hfst transducer methods mutate the object they're called on!
-            # so we need to make explicit copies.
-            target_to_child = hfst.HfstTransducer(target.converters[self])
-            target_to_child.compose(derivation)
-            target.converters[child] = target_to_child
-            child_to_target = hfst.HfstTransducer(derivation)
-            child_to_target.invert()
-            child_to_target.compose(self.converters[target])
-            child.converters[target] = child_to_target
+            target.converters[child] = pynini.compose(
+                target.converters[self], derivation)
+            child.converters[target] = pynini.compose(
+                derivation.copy().invert(), self.converters[target])
 
     def walk(self) -> None:
         """Breadth-first traversal of this level and its children.
@@ -92,12 +100,16 @@ class Level(object):
             for (childnode, childlevel) in child.walk_with_depth(level+1):
                 yield (childnode, childlevel)
 
-class Language(object):
+    def is_root(self) -> bool:
+        """True if this node is a root node."""
+        return self.parent is None
+
+class Language(Level):
     """ A language.
 
     In Cloudburst a language is made up of one or more levels of representation,
-    each with a unique name and sigil and with a set of transducers linking it to
-    other levels in the same language. Every language has a `root` level of
+    each with a unique name and sigil and with a set of transducers linking it
+    to other levels in the same language. Every language has a `root` level of
     representation. Forms that are identical at the root level are treated as
     identical for other purposes.
 
@@ -107,58 +119,46 @@ class Language(object):
     """
 
     def __init__(self,
-                 rootname: str = "root",
-                 rootsigil: str = "|") -> None:
-        self.sigils = []
-        self.levels = []
-        self.rootname = rootname
-        rootlevel = Level(name=rootname, sigil=rootsigil, language=self)
-        self.rootlevel = rootlevel
+                 root_lexicon: pynini.Fst,
+                 alphabet: pynini.Fst) -> None:
+        if isinstance(root_lexicon, str):
+            root_lexicon = pynini.acceptor(root_lexicon)
+        if isinstance(alphabet, str):
+            alphabet = pynini.acceptor(alphabet)
+        self.sigma_star = pynini.union(root_lexicon,
+                                       alphabet).closure()
+        super().__init__(name="root", derivation=root_lexicon, parent=None)
+
 
     def __repr__(self):
-        return ("Language with these levels:\n" +
-                "\n".join(
-                    [((depth * " ") + level.__repr__())
-                     for (level, depth)
-                     in self.rootlevel.walk_with_depth()]))
+        return self.name
 
-    def register_level(self, level: Level, name: str, sigil: str) -> None:
+    def register_level(self, level: Level) -> None:
         """ Called by a newly created level to inform the language of its
         name and sigil."""
-        self.levels.append(level)
-        self.sigils.append(sigil)
-        self.__dict__[name] = level
+        self.__dict__[level.name] = level
 
-
-    def has_sigil(self, sigil: str):
-        """ Is `sigil` already in use as a sigil in this language? """
-        return sigil in self.sigils
 
     def has_name(self, name: str):
         """ Is `name` already in use in this language, either as a level name
         or as the name of a built-in property or method? """
         return name in dir(self)
 
+
 class Error(Exception):
     """ Generic error for this module. """
     pass
 
 if __name__ == "__main__":
-    lang = Language(rootname="a", rootsigil="|")
-    lang.a.add_child(name="b", sigil="'", derivation=
-                     hfst.regex("a:a b:b c:c;"))
-    lang.b.add_child(name="c", sigil="!", derivation=
-                     hfst.regex("b -> d || a_a;"))
-    print(lang.a.converters[lang.c].extract_paths(output="text"))
-
-"""
-o = skoa.orthography("xetliqina")
-p = skoa.orthography.converters[skoa.pronunciation](x)
-p = o.convert_to(skoa.pronunciation)   # Should be a synonym
-
-# This should do the same as the above, though "with" isn't the right keyword.
-with skoa:
-    o = orthography("xetliqina")
-    p = orthography.converters[pronunciation](x)
-    p = o.convert_to(pronunciation)
-"""
+    l = Language(root_lexicon=(u(*"cat fox".split())+
+                               u(*"[+Sg] [+Pl]".split())),
+                 alphabet=u(*"""a b c d e f g h i j k l m n o p q r s
+                            t u v w x y z""".split()))
+    l.add_child(name="b", derivation=(pynini.cdrewrite(t("cat[+Pl]",
+                                                         "cats"),
+                                                       "","",l.sigma_star) *
+                                      pynini.cdrewrite(t("fox[+Pl]", "foxes"),
+                                                       "","",l.sigma_star)))
+    l.b.add_child(name="c", derivation=pynini.cdrewrite(t("[+Sg]",
+                                                          ""),"","",l.sigma_star))
+    print((a("fox[+Pl]")* l.converters[l.b]).stringify())
